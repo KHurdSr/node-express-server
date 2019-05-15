1fc4e217436d6fe40913ee03c1f843ac04b49b87
khurdsr <korilhurdsr@gmail.com>
2019-05-14 20:23:52 -0700

    update

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [{
+    "name": "Ray Ray",
+    "photo": "https://en.wikipedia.org/wiki/Ray_Liotta#/media/File:RayLiottaTIFFSept2012.jpg",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+},
+{
+    "name": "Eva",
+    "photo": "https://en.wikipedia.org/wiki/Eva_Longoria#/media/File:Eva_Longoria_Cannes_2015.jpg",
+    "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+},
+{
+    "name": "Jay",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/9/9c/Jay-Z_%40_Shawn_%27Jay-Z%27_Carter_Foundation_Carnival_%28crop_2%29.jpg",
+    "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+},
+{
+    "name": "Alanna",
+    "photo": "https://en.wikipedia.org/wiki/Alanna_Masterson#/media/File:Alanna_Masterson_by_Gage_Skidmore.jpg",
+    "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+},
+{
+    "name": "Jasmin",
+    "photo": "https://en.wikipedia.org/wiki/Irina_Dubtsova#/media/File:%D0%98%D1%80%D0%B8%D0%BD%D0%B0_%D0%94%D1%83%D0%B1%D1%86%D0%BE%D0%B2%D0%B0_%D0%B2_%D0%B6%D1%8E%D1%80%D0%B8_%D0%94%D0%B5%D1%82%D1%81%D0%BA%D0%BE%D0%B9_%D0%9D%D0%BE%D0%B2%D0%BE%D0%B9_%D0%B2%D0%BE%D0%BB%D0%BD%D1%8B_2016.jpg",
+    "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+},
+{
+    "name": "Nichole",
+    "photo": "https://en.wikipedia.org/wiki/Nicole_Kidman#/media/File:Nicole_Kidman_Cannes_2017_4.jpg",
+    "scores": [3, 3, 4, 1, 4, 3, 5, 3, 5, 4]
+},
+{
+    "name": "Jace",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/5/5f/Mark_Wahlberg_2017.jpg",
+    "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+}
+
+]
+
+module.exports = friends;
